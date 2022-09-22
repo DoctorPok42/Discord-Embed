e6d8381cc2268f814ad21117ad71c9c57dbea98c
DoctorPok <58393254+DoctorPok42@users.noreply.github.com>
2022-09-23 00:00:52 +0200

    Update index.ts

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,5 @@
-export { default as Embed } from './Embed';
-export { default as Field } from './Field';
-export { default as InputColor } from './InputColor';
-export { default as InputText } from './InputText';
+export { default as Embed } from "./Embed";
+export { default as Field } from "./Field";
+export { default as InputColor } from "./InputColor";
+export { default as InputText } from "./InputText";
+export { default as Thumbnail } from "./Thumbnail";

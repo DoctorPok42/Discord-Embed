6786b70f9ba78ed6b97c53de1d3ec215b1d2d6c9
DoctorPok <58393254+DoctorPok42@users.noreply.github.com>
2022-09-23 00:01:20 +0200

    Update field.tsx

--- components/Embed/field.tsx
+++ components/Embed/field.tsx
@@ -1,13 +1,19 @@
 import styles from "./styles.module.scss";
 
 interface EmbedProps {
-  name: string;
+  txt: string;
   value: string;
   alone?: boolean;
 }
 
-const Field = ({ name, value, alone = false }: EmbedProps) => {
-  return <main className={styles.container}>{name}</main>;
+const Field = ({ txt, value, alone = false }: EmbedProps) => {
+  return (
+    <main className={styles.field}>
+      <h2>{txt}</h2>
+      <br />
+      <h2>{value}</h2>
+    </main>
+  );
 };
 
 export default Field;

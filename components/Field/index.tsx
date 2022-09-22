4bbe17867ea58437023516507a12cd31ceffc8c3
DoctorPok <58393254+DoctorPok42@users.noreply.github.com>
2022-09-23 00:01:06 +0200

    Update index.tsx

--- components/Field/index.tsx
+++ components/Field/index.tsx
@@ -1,32 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./styles.module.scss";
 
 interface FieldProps {
-  name: string | number;
   txt: string;
   value: string;
-  onChange: (args: string) => void;
+  id: number
+  onChange: (newField: { txt: string; value: string; id: number }) => void;
 }
 
-const Field = ({ onChange, name }: FieldProps) => {
-  const handleChange = (args: string) => {
-    onChange(args);
+const Field = ({ onChange, txt, value, id }: FieldProps) => {
+  const [stxt, setTxt] = useState<string>(txt);
+  const [svalue, setValue] = useState<string>(value);
+
+  useEffect(() => {
+    onChange({ txt: stxt, value: svalue, id });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [stxt, svalue]);
+
+  const handleChange = (args: string, value: string) => {
+    switch (args) {
+      case "txt":
+        setTxt(value);
+        break;
+      case "value":
+        setValue(value);
+        break;
+    }
   };
   return (
     <main className={styles.container}>
-      <h2>{name} : </h2>
-      <input
-        type="text"
-        maxLength={256}
-        placeholder={`Entrer un texte`}
-        onChange={(e) => handleChange(e.target.value)}
-      />
-      <input
-        type="text"
-        maxLength={1024}
-        placeholder={`Entrer une value (optionnel)`}
-        onChange={(e) => handleChange(e.target.value)}
-      />
+      <div className={styles.flexbox}>
+        <h2>Text</h2>
+        <input
+          type="text"
+          maxLength={256}
+          placeholder={`Entrer un texte`}
+          onChange={(e) => handleChange("txt", e.target.value)}
+        />
+      </div>
+      <div className={styles.flexbox}>
+        <h2>Value</h2>
+        <input
+          type="text"
+          maxLength={1024}
+          placeholder={`Entrer une value (optionnel)`}
+          onChange={(e) => handleChange("value", e.target.value)}
+        />
+      </div>
     </main>
   );
 };

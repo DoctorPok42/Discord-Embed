2a1b10b566226f763a3d6a3d773e703a62811d73
DoctorPok42 <58393254+DoctorPok42@users.noreply.github.com>
2022-10-02 20:30:02 +0200

    Update index.tsx

--- components/InputText/index.tsx
+++ components/InputText/index.tsx
@@ -4,20 +4,38 @@ import styles from "./styles.module.scss";
 interface InputTextProps {
   name: string;
   onChange: (args: string) => void;
+  maxLength?: number;
+  autoFocus?: boolean;
 }
 
-const InputText = ({ onChange, name }: InputTextProps) => {
+const InputText = ({
+  onChange,
+  name,
+  maxLength,
+  autoFocus = false,
+}: InputTextProps) => {
+  const [value, setValue] = useState<string>("");
   const handleChange = (args: string) => {
+    setValue(args);
     onChange(args);
   };
   return (
     <main className={styles.container}>
-      <h2>{name} : </h2>
+      <h2>
+        {name} :{" "}
+        {maxLength && (
+          <span>
+            {value.length} / {maxLength}
+          </span>
+        )}
+      </h2>
       <input
         type="text"
         placeholder={`Enter ${name}`}
         onChange={(e) => handleChange(e.target.value)}
         className={name === "description" ? styles.description : ""}
+        maxLength={maxLength}
+        autoFocus={autoFocus}
       />
     </main>
   );
